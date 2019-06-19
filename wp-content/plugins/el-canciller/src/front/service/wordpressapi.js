79036a234d616f928498d3cb75dca422b51bf89e
Ian Lucas Diaz <ianlucasdiaz95@gmail.com>
2019-06-19 20:58:18 -0300

    asd

--- wp-content/plugins/el-canciller/src/front/service/wordpressapi.js
+++ wp-content/plugins/el-canciller/src/front/service/wordpressapi.js
@@ -9,10 +9,10 @@ function getData(query) {
 
     const url = `http://142.93.24.13/wp-json/wp/v2${ query }`;
 
-    const headers = new HttpHeaders({
+    const headers = {
         // tslint:disable-next-line:max-line-length
         //'Authorization': 'Bearer BQDN8FI-G3-thKplSnuymOZA8ixIHLoEnrEg4-nvcCsN64BGpyNv1LdbM53gz0ODqo9QXYLHKtbKaG7DLl0'
-    });
+    };
 
     return fetch(url, { headers }).then(data => data.json());
 
