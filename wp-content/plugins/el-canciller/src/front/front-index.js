79591c503c9e6162d14c57c5cc2a167154dfced9
Ian Lucas Diaz <ianlucasdiaz95@gmail.com>
2019-06-19 16:54:04 -0300

    test

--- wp-content/plugins/el-canciller/src/front/front-index.js
+++ wp-content/plugins/el-canciller/src/front/front-index.js
@@ -5,7 +5,7 @@
  */
 const front = require('./components/front-test');
 
-front.log('Here is a message for the frontend!');
+front.log('Here is a message for the frontend! Hola');
 
 // Let's test a function using Lodash.
 front.log(front.getLastArrayElement([1, 2, 3])); // Should log out 3.
