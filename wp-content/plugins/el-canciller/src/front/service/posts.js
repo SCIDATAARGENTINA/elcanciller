9174b7a5e40a00c93e42aa502335170162f80150
Ian Lucas Diaz <ianlucasdiaz95@gmail.com>
2019-06-19 17:32:23 -0300

    posts

--- wp-content/plugins/el-canciller/src/front/service/posts.js
+++ wp-content/plugins/el-canciller/src/front/service/posts.js
@@ -8,5 +8,6 @@ const postUrl = 'http://142.93.24.13/wp-json/wp/v2/posts';
 
 const posts = fetch(postUrl)
                 .then((resp) => resp.json());
+                .then(data => console.log(JSON.stringify(data)))
 
 module.export = posts;
