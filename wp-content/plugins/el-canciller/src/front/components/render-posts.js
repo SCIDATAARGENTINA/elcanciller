fdd93e5bff597eb84da7b3cfb88dcb79f8cc5c33
Ian Lucas Diaz <ianlucasdiaz95@gmail.com>
2019-06-19 21:21:51 -0300

    asd

--- wp-content/plugins/el-canciller/src/front/components/render-posts.js
+++ wp-content/plugins/el-canciller/src/front/components/render-posts.js
@@ -7,7 +7,7 @@
 
 function renderTemplate(data) {
 
-    var postTemplate = `<h2>${title}<h2>`;
+    //var postTemplate = `<h2>${title}<h2>`;
 
     data.forEach((key, value) => {
         console.log(key + ' - ' + value); // key - value
