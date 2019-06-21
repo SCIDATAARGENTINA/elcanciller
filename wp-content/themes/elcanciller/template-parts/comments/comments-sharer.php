547813a1829625bbe8ccc2b673d66b70982703d1
Ian Lucas Diaz <ianlucasdiaz95@gmail.com>
2019-06-21 16:34:21 -0300

    asd

--- wp-content/themes/elcanciller/template-parts/comments/comments-sharer.php
+++ wp-content/themes/elcanciller/template-parts/comments/comments-sharer.php
@@ -32,7 +32,7 @@
                   <?php echo '@' . $comment->comment_author ?>
                </span>
                <span class="comment-text">
-                  <?php echo substrwords($comment->comment_content ,2); ?>
+                  <?php echo substrwords($comment->comment_content ,1); ?>
                </span>
             </div>
 
