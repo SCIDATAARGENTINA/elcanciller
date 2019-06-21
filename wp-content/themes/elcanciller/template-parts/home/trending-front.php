bfeb777b33873a6852eb297265c139f657a832f8
Ian Lucas Diaz <ianlucasdiaz95@gmail.com>
2019-06-21 10:37:28 -0300

    trending front layout

--- wp-content/themes/elcanciller/template-parts/home/trending-front.php
+++ wp-content/themes/elcanciller/template-parts/home/trending-front.php
@@ -12,10 +12,12 @@
 ?>
 
 <div class="trending-post">
-<?php
+   <?php
    $args = array(
       'post_type' => 'post',
       'posts_per_page' => 1,
+      'orderby' => 'date',
+      'order' => 'DESC',
       'meta_query' => array(
          array(
             'key' => 'trending',
@@ -25,15 +27,29 @@
       )
    );
    $trending_post = new WP_Query($args);
-?>
+   ?>
 
-<?php if($trending_post->have_posts()) : ?>
+   <?php if ($trending_post->have_posts()) : ?>
 
-   <?php while($trending_post->have_posts()) : $trending_post->the_post() ?>
-      <div class='title'>
-         <?php the_title(); ?>
-      </div>
-   <?php endwhile ?>
+      <?php
+      $featured_img_url = get_the_post_thumbnail_url(get_the_ID(), 'full');
+      $thumbnail_id = get_post_thumbnail_id($post->ID);
+      $alt = get_post_meta($thumbnail_id, '_wp_attachment_image_alt', true);
+      ?>
 
-<?php endif ?>
+
+      <?php while ($trending_post->have_posts()) : $trending_post->the_post() ?>
+         <div class="category">
+            <h3 class="category-name"><?php echo get_the_category(); ?></h3>
+            <span><i class="fas fa-bomb"></i></span>
+         </div>
+         <div class="entry">
+            <h2><?php the_title(); ?></h2>
+            <div class="entry-img">
+               <img src="<?php echo $featured_img_url ?>" alt="<?php echo $alt ?>">
+            </div>
+         </div>
+      <?php endwhile ?>
+
+   <?php endif ?>
 </div>
