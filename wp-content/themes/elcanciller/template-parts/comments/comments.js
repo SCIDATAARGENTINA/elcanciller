101896efa28cb7fad5fdbed18823bf38c3e9c20f
Ian Lucas Diaz <ianlucasdiaz95@gmail.com>
2019-06-21 15:56:11 -0300

    asd

--- wp-content/themes/elcanciller/template-parts/comments/comments.js
+++ wp-content/themes/elcanciller/template-parts/comments/comments.js
@@ -5,12 +5,13 @@ jQuery(document).ready(function($) {
     var comentarios = $('.comentarios');
 
     comentarios.each(function() {
+        var commentsLenght = 0;
         var comentarioChildren = $(this).children();
         console.log(comentarioChildren);
         comentarioChildren.each(function() {
             commentsLenght = commentsLenght + $(this).width();
         });
-        commentsLenght = commentsLenght + 50; // Fix double line text
+        commentsLenght = commentsLenght + 100; // Fix double line text
         $(this).css('width', commentsLenght + 'px');
     });
 
