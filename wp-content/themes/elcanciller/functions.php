24f913eb9dc61b404672387f8bb6dfeed4bca634
Ian Lucas Diaz <ianlucasdiaz95@gmail.com>
2019-06-24 13:47:55 -0300

    asd

--- wp-content/themes/elcanciller/functions.php
+++ wp-content/themes/elcanciller/functions.php
@@ -5,11 +5,7 @@
 //
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
-    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-    wp_enqueue_style( 'child-style',
-        get_stylesheet_directory_uri() . '/style.css',
-        array('parent-style')
-    );
+
 }
 //
 // Your code goes below
@@ -123,3 +119,10 @@ function substrwords($text, $maxchar, $end = '...'){
   }
   return $output;
 }
+
+//Dequeue Styles
+function project_dequeue_unnecessary_styles() {
+    wp_dequeue_style( 'bootstrap-map' );
+        wp_deregister_style( 'bootstrap-map' );
+}
+add_action( 'wp_print_styles', 'project_dequeue_unnecessary_styles' );
