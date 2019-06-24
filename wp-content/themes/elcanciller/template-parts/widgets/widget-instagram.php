f427a2681d5f2009651eb0cd655559e22fad41b1
Ian Lucas Diaz <ianlucasdiaz95@gmail.com>
2019-06-24 16:35:16 -0300

    widget el canciller live

--- wp-content/themes/elcanciller/template-parts/widgets/widget-instagram.php
+++ wp-content/themes/elcanciller/template-parts/widgets/widget-instagram.php
@@ -12,5 +12,16 @@
 ?>
 
 <div class="instagram-widget container">
+    <div class="instagram-profile">
+        <div class="instagram-pic">
+            <a href="https://www.instagram.com/elcancillerlive/"><img src="<?php bloginfo('url') ?>/wp-content/uploads/2019/06/cancillerlivelogo2.svg" alt="El Canciller Live"></a>
+        </div>
+        <div class="instagram-data">
+            <h4><a href="https://www.instagram.com/elcancillerlive/">@elcancillerlive</a></h4>
+            <hr>
+            <p>El Canciller Live</p>
+            <span><i class="far fa-envelope"></i>ms@elcanciller.com</span>
+        </div>
+    </div><!-- end instagram profile -->
     <div class="feed"><?php echo do_shortcode('[instagram-feed]'); ?></div>
 </div><!-- end instagram widget container -->
