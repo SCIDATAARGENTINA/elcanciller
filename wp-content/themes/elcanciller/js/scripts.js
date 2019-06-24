08864b9f6132ff4c7a00877aa3c7e7b949cda360
Ian Lucas Diaz <ianlucasdiaz95@gmail.com>
2019-06-24 11:29:01 -0300

    sfsd

--- wp-content/themes/elcanciller/js/scripts.js
+++ wp-content/themes/elcanciller/js/scripts.js
@@ -23,7 +23,22 @@ jQuery(document).ready(function($) {
         }) // end ajax
 
 
-    $(".cancilleram").owlCarousel();
+    $(".cancilleram").owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 5
+            }
+        }
+    });
 
 
 });
