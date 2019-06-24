3dd8d3f8fdef0a748b66c8ff544971f61a6ab50d
Ian Lucas Diaz <ianlucasdiaz95@gmail.com>
2019-06-24 10:31:19 -0300

    add owl carrousel css & js

--- wp-content/themes/elcanciller/functions.php
+++ wp-content/themes/elcanciller/functions.php
@@ -19,6 +19,9 @@ function custom_scripts() {
   wp_enqueue_script( 'custom-js', get_stylesheet_directory_uri() . '/js/scripts.js', array( 'jquery' ), '1.0.0', true );
   wp_enqueue_script( 'skycons-js', get_stylesheet_directory_uri() . '/js/skycons.js', array( 'jquery' ), '1.0.0', true );
   wp_enqueue_script('comments-js', get_stylesheet_directory_uri() . '/template-parts/comments/comments.js', array('jquery'), '1.0.0', true);
+  wp_enqueue_script('owl-js', get_stylesheet_directory_uri() . '/owl/owl.carousel.min.js', array('jquery'), '1.0.0', true);
+  wp_enqueue_style( 'owl-css', get_stylesheet_directory_uri() . '/owl/owl.carousel.min.css' );
+  wp_enqueue_style( 'owl-theme-css', get_stylesheet_directory_uri() . '/owl/owl.theme.default.min.css' );
 
 }
 add_action( 'wp_enqueue_scripts', 'custom_scripts' );
