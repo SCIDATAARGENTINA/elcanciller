41205d050b160aa5ca3e8c4b146f51866cb1ceab
Ian Lucas Diaz <ianlucasdiaz95@gmail.com>
2019-06-26 15:44:41 -0300

    dsfsdf

--- wp-content/plugins/el-canciller/src/utils/utils-index.js
+++ wp-content/plugins/el-canciller/src/utils/utils-index.js
@@ -6,7 +6,7 @@
  * Suffle Array ( Para randomizar los posts);
  */
 
-let shuffle = function(array) {
+let shuffle = (array) => {
 
     let currentIndex = array.length;
     let temporaryValue, randomIndex;
