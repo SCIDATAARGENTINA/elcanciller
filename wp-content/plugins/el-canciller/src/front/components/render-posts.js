0b7041fbf59fc7eeab8a00ea79c790f558935932
Ian Lucas Diaz <ianlucasdiaz95@gmail.com>
2019-06-26 15:52:45 -0300

    asdasd

--- wp-content/plugins/el-canciller/src/front/components/render-posts.js
+++ wp-content/plugins/el-canciller/src/front/components/render-posts.js
@@ -17,7 +17,6 @@ let renderTemplate = (postArr) => {
     for (let node of renderNodes) {
 
         let random = node.getAttribute('data-random');
-        console.log(postArr);
         console.log(random);
         if (random == 1) {
             postArr = shuffle(postArr);
