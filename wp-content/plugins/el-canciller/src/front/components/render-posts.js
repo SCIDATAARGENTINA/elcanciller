a0e7a386061d93ffaddc8ab53e2ad599ad4e8979
Ian Lucas Diaz <ianlucasdiaz95@gmail.com>
2019-06-28 14:57:18 -0300

    asd

--- wp-content/plugins/el-canciller/src/front/components/render-posts.js
+++ wp-content/plugins/el-canciller/src/front/components/render-posts.js
@@ -8,14 +8,23 @@ var moment = require('moment');
 
 var utils = require('../../utils/utils-index');
 
-require('moment/locale/es'); // without this line it didn't work
+require('moment/locale/es');
 moment.locale('es');
 
 
 
 let setTemplate = (post) => {
 
-    let template = '';
+    if (post.trending == 'si') {
+        let template = '<div class="post-rendered trending"></div>';
+    } else {
+        let template = '<div class="post-rendered"></div>';
+    }
+
+    template.append('Hola');
+
+    console.log(template);
+
 
 
 };
