b2a41eea6a156406aad435e30159506efd1e133b
Ian Lucas Diaz <ianlucasdiaz95@gmail.com>
2019-07-03 12:17:24 -0300

    asd

--- wp-content/themes/elcanciller/template-parts/sections/section-videos.php
+++ wp-content/themes/elcanciller/template-parts/sections/section-videos.php
@@ -20,7 +20,7 @@
                     <a href="https://www.instagram.com/elcancillerlive/"><img src="<?php bloginfo('url') ?>/wp-content/uploads/2019/07/videos-logo.svg" alt="El CancillerVideos"></a>
                 </div><!-- end videos-pic -->
                 <div class="videos-data">
-                    <h4><a href="https://www.instagram.com/elcancillerlive/">Streaming</a></h4>
+                    <h4><a href="#">Streaming</a></h4>
                     <a href="#"><p>elcancillercom/videos/</p></a>
                 </div><!-- end videos-data -->
             </div><!-- end videos-title -->
