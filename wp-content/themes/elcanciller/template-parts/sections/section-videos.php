bb3f0b3d6fcff729e6a566192cb4a7b8451f3d63
Ian Lucas Diaz <ianlucasdiaz95@gmail.com>
2019-07-03 12:40:49 -0300

    asd

--- wp-content/themes/elcanciller/template-parts/sections/section-videos.php
+++ wp-content/themes/elcanciller/template-parts/sections/section-videos.php
@@ -25,13 +25,34 @@
                 </div><!-- end videos-data -->
             </div><!-- end videos-title -->
             <div class="videos-list">
-
+                
             </div><!-- end logo -->
         </div><!-- end profile data -->
     </div><!-- videos-title-list -->
 
     <div class="videos-feed">
+        <?php $args = array(
+                        'posts_per_page' => 2,
+                        'post_type' => 'videos'
+                    );
+                    $query = new WP_Query( $args );
+                    while( $query->have_posts() ) {
+                        $query->the_post(); 
+                        $featured_img_url = get_the_post_thumbnail_url(get_the_ID(), 'medium');
+                        $thumbnail_id = get_post_thumbnail_id($post->ID);
+                        $alt = get_post_meta($thumbnail_id, '_wp_attachment_image_alt', true);
+                        ?>
+                        
+                        <div class="video" style="background-image: url('<?php echo $featured_img_url ?>');">
+                            <div class="player-icon">
+                                <img src="<?php bloginfo('url') ?>/wp-content/uploads/2019/07/videos-icon.svg" alt="<?php echo $alt ?>">
+                            </div><!-- player icon -->
+                        </div><!-- video -->
 
+                    <?php 
+                    }
+                    // Restore original Post Data
+                    wp_reset_postdata(); ?> 
     </div><!-- end videos-feed -->
     
 </div><!-- end videos widget container -->
