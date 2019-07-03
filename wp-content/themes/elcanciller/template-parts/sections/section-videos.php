0b975eab950490f3a86571ee87322820c46767ee
Ian Lucas Diaz <ianlucasdiaz95@gmail.com>
2019-07-03 12:02:32 -0300

    asdasd

---
+++ wp-content/themes/elcanciller/template-parts/sections/section-videos.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Template part for displaying CANCILLER SECCION VIDEOS
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package WordPress
+ * @subpackage Twenty_Nineteen
+ * @since 1.0.0
+ */
+
+?>
+
+<div class="videos-section container">
+
+
+    
+</div><!-- end videos widget container -->
