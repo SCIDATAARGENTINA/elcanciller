aef026e64451cac33286763caea79b2ed8fc907b
Ian Lucas Diaz <ianlucasdiaz95@gmail.com>
2019-07-03 15:12:51 -0300

    asdasd

--- wp-content/themes/elcanciller/js/scripts.js
+++ wp-content/themes/elcanciller/js/scripts.js
@@ -72,7 +72,7 @@ jQuery(document).ready(function($) {
 
     $('.popup-video').each(function() {
         var id = $(this).attr('data-id');
-        var popupContent = ('#video-popup-' + id + '').html();
+        var popupContent = $('#video-popup-' + id + '').html();
         console.log(popupContent);
         $(this).magnificPopup({
             items: {
