570489c73124f1fa10e0b803bf529274215d218a
Ian Lucas Diaz <ianlucasdiaz95@gmail.com>
2019-07-03 10:30:48 -0300

    asd

--- wp-content/themes/elcanciller/template-parts/sections/section-instagram.php
+++ wp-content/themes/elcanciller/template-parts/sections/section-instagram.php
@@ -12,19 +12,21 @@
 ?>
 
 <div class="instagram-section container">
-    <div class="instagram-profile">
-        <div class="instagram-pic">
-            <a href="https://www.instagram.com/elcancillerlive/"><img src="<?php bloginfo('url') ?>/wp-content/uploads/2019/06/cancillerlivelogo2.svg" alt="El Canciller Live"></a>
-        </div>
-        <div class="instagram-data">
-            <h4><a href="https://www.instagram.com/elcancillerlive/">@elcancillerlive</a></h4>
-            <hr>
-            <p>El Canciller Live</p>
-            <span><i class="far fa-envelope"></i>ms@elcanciller.com</span>
-        </div>
+    <div class="profile-data">
+        <div class="instagram-profile">
+            <div class="instagram-pic">
+                <a href="https://www.instagram.com/elcancillerlive/"><img src="<?php bloginfo('url') ?>/wp-content/uploads/2019/06/cancillerlivelogo2.svg" alt="El Canciller Live"></a>
+            </div><!-- end profile pic -->
+            <div class="instagram-data">
+                <h4><a href="https://www.instagram.com/elcancillerlive/">@elcancillerlive</a></h4>
+                <hr>
+                <p>El Canciller Live</p>
+                <span><i class="far fa-envelope"></i>ms@elcanciller.com</span>
+            </div><!-- end data -->
+        </div><!-- end instagram profile -->
         <div class="instagram-logo">
-            <img src="<?php bloginfo('url') ?>/wp-content/uploads/2019/07/instagram-section-logo.png " alt="Instagram - El Canciller Live">
-        </div>
-    </div><!-- end instagram profile -->
+                <img src="<?php bloginfo('url') ?>/wp-content/uploads/2019/07/instagram-section-logo.png " alt="Instagram - El Canciller Live">
+        </div><!-- end logo -->
+    </div><!-- end profile data -->
     <div class="feed"><?php echo do_shortcode('[instagram-feed]'); ?></div>
 </div><!-- end instagram widget container -->
