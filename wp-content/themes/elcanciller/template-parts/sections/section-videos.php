89bbbcab4faf75bd637531a5bd645f7b02cfdfda
Ian Lucas Diaz <ianlucasdiaz95@gmail.com>
2019-07-03 17:01:12 -0300

    adasd

--- wp-content/themes/elcanciller/template-parts/sections/section-videos.php
+++ wp-content/themes/elcanciller/template-parts/sections/section-videos.php
@@ -29,9 +29,9 @@
                         'posts_per_page' => 2,
                         'post_type' => 'video'
                     );
-                    $query = new WP_Query( $args );
-                    while( $query->have_posts() ) {
-                        $query->the_post();
+                    $query1 = new WP_Query( $args );
+                    while( $query1->have_posts() ) {
+                        $query1->the_post();
                         $categories = get_the_terms( $post->ID , array( 'categoria_videos') );
                         $term_link = get_term_link( $categories[0], array( 'categoria_videos') );
                         ?>
