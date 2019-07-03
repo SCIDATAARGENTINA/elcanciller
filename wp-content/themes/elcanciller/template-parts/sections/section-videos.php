c6727388929425134e766e392a3fe37a23133ea9
Ian Lucas Diaz <ianlucasdiaz95@gmail.com>
2019-07-03 17:13:57 -0300

    asdasd

--- wp-content/themes/elcanciller/template-parts/sections/section-videos.php
+++ wp-content/themes/elcanciller/template-parts/sections/section-videos.php
@@ -25,40 +25,7 @@
                 </div><!-- end videos-data -->
             </div><!-- end videos-title -->
             <div class="videos-list">
-                <?php $args = array(
-                        'posts_per_page' => -1,
-                        'post_type' => 'video'
-                    );
-                    $query = new WP_Query( $args );
-                    while( $query->have_posts() ) {
-                        $query->the_post();
-                        $categories = get_the_terms( $post->ID , array( 'categoria_videos') );
-                        $term_link = get_term_link( $categories[0], array( 'categoria_videos') );
-                        ?>
-                        
-                        <div class="video-list-title">
-                            <div class="">
-                                <h3><?php the_title(); ?></h3>
-                            </div>
-                            <div id="video-popup-<?php echo $post->ID ?>" class="player-content mfp-hide">
-                                <div class="popup-content">
-                                    <div class="videos-title">
-                                        <div class="videos-pic">
-                                            <a href="#"><img src="<?php bloginfo('url') ?>/wp-content/uploads/2019/07/videos-logo.svg" alt="El Canciller - Videos"></a>
-                                        </div><!-- end videos-pic -->
-                                        <div class="videos-data">
-                                            <h4><a href="<?php echo $term_link ?>"><?php echo $categories[0]->name ?></a></h4>
-                                            <h3><?php the_title(); ?></h3>
-                                        </div><!-- end videos-data -->
-                                    </div><!-- end videos-title -->
-                                    <?php echo get_the_content(); ?>
-                                </div>
-                            </div>
-                        </div><!-- video-list-title -->
-                        
-                    <?php
-                    rewind_posts(); }
-                    ?> 
+                
             </div><!-- end videos-list -->
         </div><!-- end profile data -->
     </div><!-- videos-title-list -->
