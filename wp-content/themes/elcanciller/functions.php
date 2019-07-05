5fc76ac1c00d193ffb18ee385f1d9f8c835754f3
Ian Lucas Diaz <ianlucasdiaz95@gmail.com>
2019-07-05 15:27:19 -0300

    zxczxc

--- wp-content/themes/elcanciller/functions.php
+++ wp-content/themes/elcanciller/functions.php
@@ -158,11 +158,66 @@ function custom_post_type_videos()
 }
 add_action('init', 'custom_post_type_videos', 0);
 
+function custom_post_type_opinion()
+{
+
+  $labels = array(
+    'name'                  => _x('Opiniones', 'Post Type General Name', 'elcanciller'),
+    'singular_name'         => _x('Opinión', 'Post Type Singular Name', 'elcanciller'),
+    'menu_name'             => __('Opinión', 'elcanciller'),
+    'name_admin_bar'        => __('Opinión', 'elcanciller'),
+    'archives'              => __('Archivo', 'elcanciller'),
+    'attributes'            => __('Atributos', 'elcanciller'),
+    'parent_item_colon'     => __('Padre:', 'elcanciller'),
+    'all_items'             => __('Todos los items', 'elcanciller'),
+    'add_new_item'          => __('Agregar nuevo', 'elcanciller'),
+    'add_new'               => __('Agregar nuevo', 'elcanciller'),
+    'new_item'              => __('Nuevo', 'elcanciller'),
+    'edit_item'             => __('Editar', 'elcanciller'),
+    'update_item'           => __('Actualizar', 'elcanciller'),
+    'view_item'             => __('Ver', 'elcanciller'),
+    'view_items'            => __('Ver', 'elcanciller'),
+    'search_items'          => __('Buscar', 'elcanciller'),
+    'not_found'             => __('No encontrado', 'elcanciller'),
+    'not_found_in_trash'    => __('No encontrado en la papelera', 'elcanciller'),
+    'featured_image'        => __('Imagen destacada', 'elcanciller'),
+    'set_featured_image'    => __('Agregar imagen destacada', 'elcanciller'),
+    'remove_featured_image' => __('Borrar imagen destacada', 'elcanciller'),
+    'use_featured_image'    => __('Usar como imagen destacada', 'elcanciller'),
+    'insert_into_item'      => __('Insertar', 'elcanciller'),
+    'uploaded_to_this_item' => __('Cargado', 'elcanciller'),
+    'items_list'            => __('Listado', 'elcanciller'),
+    'items_list_navigation' => __('Navegacion del listado', 'elcanciller'),
+    'filter_items_list'     => __('Filtrar listado', 'elcanciller'),
+  );
+  $args = array(
+    'label'                 => __('Opinión', 'elcanciller'),
+    'description'           => __('Modulo de administración de Opinión', 'elcanciller'),
+    'labels'                => $labels,
+    'supports'              => array('title', 'editor', 'excerpt', 'author', 'thumbnail', 'comments', 'revisions'),
+    'taxonomies'            => array('category', 'tags'),
+    'hierarchical'          => false,
+    'public'                => true,
+    'show_ui'               => true,
+    'show_in_menu'          => true,
+    'menu_position'         => 6,
+    'show_in_admin_bar'     => false,
+    'show_in_nav_menus'     => false,
+    'can_export'            => true,
+    'has_archive'           => false,
+    'exclude_from_search'   => true,
+    'publicly_queryable'    => false,
+    'capability_type'       => 'page',
+  );
+  register_post_type('opinion', $args);
+}
+add_action('init', 'custom_post_type_opinion', 0);
+
 //Crear taxonomia personalizada para videos.
-add_action( 'init', 'crunchify_create_deals_custom_taxonomy', 0 );
+add_action( 'init', 'cat_videos_custom_taxonomy', 0 );
  
 //Crear taxonomia personalizada para videos.
-function crunchify_create_deals_custom_taxonomy() {
+function cat_videos_custom_taxonomy() {
  
   $labels = array(
     'name' => _x( 'Categorias', 'taxonomy general name' ),
