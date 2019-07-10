3aea8e799fc2780ff8dc140458cc737d0f091046
Ian Lucas Diaz <ianlucasdiaz95@gmail.com>
2019-07-10 14:42:13 -0300

    asd

--- wp-content/themes/elcanciller/template-parts/content/content-opinion.php
+++ wp-content/themes/elcanciller/template-parts/content/content-opinion.php
@@ -24,7 +24,7 @@ $term_link = get_term_link( $categories[0], array( 'categoria_videos') );
         <img src="<?php echo $featured_img_url ?>" alt="<?php echo $alt ?>">
     </div>
     <div class="autor">
-        <p><?php the_author(); ?></p>
+        <p>@<?php the_author(); ?></p>
     </div>
     <div class="titulo">
         <h3><?php the_title(); ?></h3>
