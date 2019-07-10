d45bee7ee71e0b686616d4809a4b2f65a7df8626
Ian Lucas Diaz <ianlucasdiaz95@gmail.com>
2019-07-10 15:04:58 -0300

    asd

--- wp-content/themes/elcanciller/functions.php
+++ wp-content/themes/elcanciller/functions.php
@@ -25,7 +25,7 @@ function custom_scripts() {
 }
 add_action( 'wp_enqueue_scripts', 'custom_scripts' );
 
-add_image_size( 'opinion', 300, 150 );
+add_image_size( 'opinion', 300, 300 );
 
 add_action( 'widgets_init', 'sidebar_register' );
 function sidebar_register() {
