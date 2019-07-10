37225239bbaaa084b862e76a16985b1c8055bbce
Ian Lucas Diaz <ianlucasdiaz95@gmail.com>
2019-07-10 16:56:13 -0300

    asd

---
+++ wp-content/themes/elcanciller/template-parts/sections/section-placas.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Template part for displaying CANCILLER SECCION VIDEOS
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package WordPress
+ * @subpackage Twenty_Nineteen
+ * @since 1.0.0
+ */
+
+?>
+
+<div class="placas-section container">
+    
+    <div class="placa-title">
+        <img src="<?php bloginfo('url') ?>/wp-content/uploads/2019/07/placa-logo.svg" alt="El Canciller Twitter">
+
+    </div><!-- placa-title -->
+    <div class="placa-image">
+        
+    </div><!-- placa-image -->
+    
+</div><!-- end placas-section container -->
