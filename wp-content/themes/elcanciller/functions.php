9935e13245c8270e397cff4691d2094d9312abe4
Ian Lucas Diaz <ianlucasdiaz95@gmail.com>
2019-07-10 16:21:39 -0300

    asdasd

--- wp-content/themes/elcanciller/functions.php
+++ wp-content/themes/elcanciller/functions.php
@@ -86,6 +86,7 @@ function custom_post_type_cancilleram()
     'public'                => true,
     'show_ui'               => true,
     'show_in_menu'          => true,
+    'menu_icon'             => 'dashicons-welcome-widgets-menus',
     'menu_position'         => 5,
     'show_in_admin_bar'     => false,
     'show_in_nav_menus'     => false,
@@ -141,6 +142,7 @@ function custom_post_type_videos()
     'public'                => true,
     'show_ui'               => true,
     'show_in_menu'          => true,
+    'menu_icon'             => 'dashicons-video-alt3',
     'menu_position'         => 6,
     'show_in_admin_bar'     => false,
     'show_in_nav_menus'     => false,
@@ -197,7 +199,7 @@ function custom_post_type_opinion()
     'show_ui'               => true,
     'show_in_menu'          => true,
     'menu_position'         => 6,
-    'menu_icon'             => 'dashicons-admin-site',
+    'menu_icon'             => 'dashicons-testimonial',
     'show_in_admin_bar'     => false,
     'show_in_nav_menus'     => false,
     'can_export'            => true,
@@ -209,6 +211,62 @@ function custom_post_type_opinion()
   register_post_type('opinion', $args);
 }
 add_action('init', 'custom_post_type_opinion', 0);
+
+function custom_post_type_placa()
+{
+
+  $labels = array(
+    'name'                  => _x('Placas', 'Post Type General Name', 'elcanciller'),
+    'singular_name'         => _x('Placa', 'Post Type Singular Name', 'elcanciller'),
+    'menu_name'             => __('Placa', 'elcanciller'),
+    'name_admin_bar'        => __('Placa', 'elcanciller'),
+    'archives'              => __('Archivo', 'elcanciller'),
+    'attributes'            => __('Atributos', 'elcanciller'),
+    'parent_item_colon'     => __('Padre:', 'elcanciller'),
+    'all_items'             => __('Todos los items', 'elcanciller'),
+    'add_new_item'          => __('Agregar nuevo', 'elcanciller'),
+    'add_new'               => __('Agregar nuevo', 'elcanciller'),
+    'new_item'              => __('Nuevo', 'elcanciller'),
+    'edit_item'             => __('Editar', 'elcanciller'),
+    'update_item'           => __('Actualizar', 'elcanciller'),
+    'view_item'             => __('Ver', 'elcanciller'),
+    'view_items'            => __('Ver', 'elcanciller'),
+    'search_items'          => __('Buscar', 'elcanciller'),
+    'not_found'             => __('No encontrado', 'elcanciller'),
+    'not_found_in_trash'    => __('No encontrado en la papelera', 'elcanciller'),
+    'featured_image'        => __('Imagen destacada', 'elcanciller'),
+    'set_featured_image'    => __('Agregar imagen destacada', 'elcanciller'),
+    'remove_featured_image' => __('Borrar imagen destacada', 'elcanciller'),
+    'use_featured_image'    => __('Usar como imagen destacada', 'elcanciller'),
+    'insert_into_item'      => __('Insertar', 'elcanciller'),
+    'uploaded_to_this_item' => __('Cargado', 'elcanciller'),
+    'items_list'            => __('Listado', 'elcanciller'),
+    'items_list_navigation' => __('Navegacion del listado', 'elcanciller'),
+    'filter_items_list'     => __('Filtrar listado', 'elcanciller'),
+  );
+  $args = array(
+    'label'                 => __('Placa', 'elcanciller'),
+    'description'           => __('Modulo de administración de Placas', 'elcanciller'),
+    'labels'                => $labels,
+    'supports'              => array('title', 'editor', 'author', 'thumbnail'),
+    'taxonomies'            => array(),
+    'hierarchical'          => false,
+    'public'                => true,
+    'show_ui'               => true,
+    'show_in_menu'          => true,
+    'menu_position'         => 6,
+    'menu_icon'             => 'dashicons-align-center',
+    'show_in_admin_bar'     => false,
+    'show_in_nav_menus'     => false,
+    'can_export'            => true,
+    'has_archive'           => false,
+    'exclude_from_search'   => true,
+    'publicly_queryable'    => true,
+    'capability_type'       => 'post',
+  );
+  register_post_type('placa', $args);
+}
+add_action('init', 'custom_post_type_placa', 0);
 
 //Crear taxonomia personalizada para videos.
 add_action( 'init', 'cat_videos_custom_taxonomy', 0 );
