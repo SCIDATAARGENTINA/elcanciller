d6b0e1b928a9c6d962b996b8b916b5b39c09535f
Ian Lucas Diaz <ianlucasdiaz95@gmail.com>
2019-07-12 12:08:57 -0300

    asd

--- wp-content/themes/elcanciller/template-parts/content/content-single.php
+++ wp-content/themes/elcanciller/template-parts/content/content-single.php
@@ -9,11 +9,26 @@
  * @since 1.0.0
  */
 
+$featured_img_url = get_the_post_thumbnail_url(get_the_ID(), 'medium');
+$thumbnail_id = get_post_thumbnail_id($post->ID);
+$alt = get_post_meta($thumbnail_id, '_wp_attachment_image_alt', true);
+$categories = get_the_terms( $post->ID , array( 'categoria_videos') );
+$term_link = get_term_link( $categories[0], array( 'categoria_videos') );
+$author_link = get_author_posts_url( get_the_author_meta('ID') );
+
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-<h1><?php the_title(); ?></h1>
-<?php the_content(); ?>
+	<img src="<?php echo $featured_img_url ?>" alt="<?php echo $alt ?>">
+
+	<div class="post-title">
+		<h1><?php the_title(); ?></h1>
+		<div class="resumen">
+			<?php echo get_the_excerpt(); ?>
+		</div><!-- resumen -->
+	</div><!-- post-title -->
+	
+	<?php the_content(); ?>
 
 </article><!-- #post-${ID} -->
 
