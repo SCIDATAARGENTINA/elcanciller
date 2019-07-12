91dffb43b4b32ae2b2f9b4a2565c6bdfbc7d2220
Ian Lucas Diaz <ianlucasdiaz95@gmail.com>
2019-07-12 15:50:41 -0300

    asd

--- wp-content/themes/elcanciller/functions.php
+++ wp-content/themes/elcanciller/functions.php
@@ -339,45 +339,28 @@ class Canciller_Walker_Comment extends Walker_Comment {
 
 		?>
 		<<?php echo $tag; ?> id="comment-<?php comment_ID(); ?>" <?php comment_class( $this->has_children ? 'parent' : '', $comment ); ?>>
-			<article id="div-comment-<?php comment_ID(); ?>" class="comment-body">
-				<footer class="comment-meta">
-					<div class="comment-author vcard">
-						<?php
-						$comment_author_url = get_comment_author_url( $comment );
-						$comment_author     = get_comment_author( $comment );
-            $avatar             = get_avatar( $comment, $args['avatar_size'] );
-            ?>
-						<span class="reply-author">@<?php echo $comment_author ?></span>
-					</div><!-- .comment-author -->
-
-					<div class="comment-metadata">
-						<a href="<?php echo esc_url( get_comment_link( $comment, $args ) ); ?>">
-							<?php
-								/* translators: 1: comment date, 2: comment time */
-								$comment_timestamp = sprintf( __( '%1$s at %2$s', 'twentynineteen' ), get_comment_date( '', $comment ), get_comment_time() );
-							?>
-							<time datetime="<?php comment_time( 'c' ); ?>" title="<?php echo $comment_timestamp; ?>">
-								<?php echo $comment_timestamp; ?>
-							</time>
-						</a>
-						<?php
-							$edit_comment_icon = twentynineteen_get_icon_svg( 'edit', 16 );
-							edit_comment_link( __( 'Edit', 'twentynineteen' ), '<span class="edit-link-sep">&mdash;</span> <span class="edit-link">' . $edit_comment_icon, '</span>' );
-						?>
-					</div><!-- .comment-metadata -->
-
-					<?php if ( '0' == $comment->comment_approved ) : ?>
-					<p class="comment-awaiting-moderation"><?php _e( 'Your comment is awaiting moderation.', 'twentynineteen' ); ?></p>
-					<?php endif; ?>
-				</footer><!-- .comment-meta -->
-
-				<div class="comment-content">
-					<?php comment_text(); ?>
-				</div><!-- .comment-content -->
-
-			</article><!-- .comment-body -->
-
-			<?php
+      <article id="div-comment-<?php comment_ID(); ?>" class="comment-body">
+        <div class="comment-actions">
+          <?php
+            $edit_comment_icon = twentynineteen_get_icon_svg( 'edit', 16 );
+            edit_comment_link( __( 'Edit', 'twentynineteen' ), '<span class="edit-link-sep">&mdash;</span> <span class="edit-link">' . $edit_comment_icon, '</span>' );
+          ?>
+          <?php if ( '0' == $comment->comment_approved ) : ?>
+          <p class="comment-awaiting-moderation"><?php _e( 'Your comment is awaiting moderation.', 'twentynineteen' ); ?></p>
+          <?php endif; ?>
+        </div><!-- .comment-metadata -->
+        <div class="comment-author">
+          <?php
+          $comment_author_url = get_comment_author_url( $comment );
+          $comment_author     = get_comment_author( $comment );
+          $avatar             = get_avatar( $comment, $args['avatar_size'] );
+          ?>
+          <span class="reply-author">@<?php echo $comment_author ?></span>
+        </div><!-- .comment-author -->
+        <div class="comment-content">
+          <?php comment_text(); ?>
+        </div><!-- .comment-content -->
+        <?php
 			comment_reply_link(
 				array_merge(
 					$args,
@@ -391,6 +374,7 @@ class Canciller_Walker_Comment extends Walker_Comment {
 				)
 			);
 			?>
+			</article><!-- .comment-body -->
 		<?php
 	}
 }
