f18ce04971c66133389bb9a3ac07ba70e54db7d3
Ian Lucas Diaz <ianlucasdiaz95@gmail.com>
2019-07-12 14:22:13 -0300

    asd

--- wp-content/themes/elcanciller/single.php
+++ wp-content/themes/elcanciller/single.php
@@ -40,12 +40,14 @@ get_header();
 
 		</div><!-- inner container -->
 		<div class="outer-container">
-			<div class="related-title">
-				<i class="fas fa-bomb"></i>
-				<h3>Lo que no te podés perder</h3>
-			</div><!-- related-title -->
-			<div class="loader">Cargando...</div>
-			<div class="render-posts col-3" data-quantity="9">
+			<div class="related-posts">
+				<div class="related-title">
+					<i class="fas fa-bomb"></i>
+					<h3>Lo que no te podés perder</h3>
+				</div><!-- related-title -->
+				<div class="loader">Cargando...</div>
+				<div class="render-posts col-3" data-quantity="9">
+				</div>
 			</div>
 		</div><!-- outer-container -->
 
