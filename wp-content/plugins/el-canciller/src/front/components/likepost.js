cc8e8889f301db84a16681f4f0941718818821a7
Ian Lucas Diaz <ianlucasdiaz95@gmail.com>
2019-07-24 11:36:50 -0300

    asd

--- wp-content/plugins/el-canciller/src/front/components/likepost.js
+++ wp-content/plugins/el-canciller/src/front/components/likepost.js
@@ -20,10 +20,10 @@ let updateLikeData = (likeCount, id, url) => {
             post_id: id,
             like_count: likeCount
         })
-    }).then(function() {
-        console.log("ok");
-    }).catch(function() {
-        console.log("error");
+    }).then(function(data) {
+        console.log(data);
+    }).catch(function(data) {
+        console.log(data);
     });
 };
 
