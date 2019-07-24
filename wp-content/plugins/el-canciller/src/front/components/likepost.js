245f3212cb44df8bb3fd187e91e88a03c7e1c25d
Ian Lucas Diaz <ianlucasdiaz95@gmail.com>
2019-07-24 10:29:00 -0300

    hjjgj

--- wp-content/plugins/el-canciller/src/front/components/likepost.js
+++ wp-content/plugins/el-canciller/src/front/components/likepost.js
@@ -1,76 +1,73 @@
 var $ = require("jquery");
 
-jQuery(document).ready(function($) {
+function getPostData(id, url) {
+    var connection = url + '/wp-json/wp/v2/posts/' + id;
+    return $.get(connection);
+}
 
-    function getPostData(id, url) {
-        var connection = url + '/wp-json/wp/v2/posts/' + id;
-        return $.get(connection);
-    }
-
-    function addLike(likeCount, id, url) {
-        $.ajax({
-            url: content_data.ajax_url,
-            type: 'POST',
-            data: {
-                action: 'ajax_call_count_satisfaccion',
-                post_id: id,
-                like_count: likeCount,
-            },
-            success: function(result) {
-                console.log(result);
-                setClapCookie(id);
-            },
-            error: function(errorThrown) {
-                console.log(errorThrown);
-            }
-        });
-
-    }
-
-    function setClapCookie(id) {
-        Cookies.set('clapped' + id, '1', { expires: 214748 });
-    }
-
-    function checkClapCookie(id) {
-        var val = Cookies.get('clapped' + id);
-        return val;
-    }
-
-    function validateClapCookie(val) {
-        if (val == 1) {
-            $('.clap').addClass('clapped');
-            $('.clap-count').addClass('added');
-            $('.share-bar').css('opacity', '1');
-        } else {
-            $('.share-bar').css('opacity', '1');
+function addLike(likeCount, id, url) {
+    $.ajax({
+        url: content_data.ajax_url,
+        type: 'POST',
+        data: {
+            action: 'ajax_call_count_satisfaccion',
+            post_id: id,
+            like_count: likeCount,
+        },
+        success: function(result) {
+            console.log(result);
+            setClapCookie(id);
+        },
+        error: function(errorThrown) {
+            console.log(errorThrown);
         }
+    });
+
+}
+
+function setClapCookie(id) {
+    Cookies.set('clapped' + id, '1', { expires: 214748 });
+}
+
+function checkClapCookie(id) {
+    var val = Cookies.get('clapped' + id);
+    return val;
+}
+
+function validateClapCookie(val) {
+    if (val == 1) {
+        $('.clap').addClass('clapped');
+        $('.clap-count').addClass('added');
+        $('.share-bar').css('opacity', '1');
+    } else {
+        $('.share-bar').css('opacity', '1');
     }
+}
 
-    let likePost = () => {
+let likePost = () => {
 
-        var url = 'https://socialnews.com.ar';
+    var url = 'https://socialnews.com.ar';
 
-        var id = $('article').attr('data-post-id');
+    var id = $('article').attr('data-post-id');
 
-        var clap = $('.like');
+    var clap = $('.like');
 
-        validateClapCookie(checkClapCookie(id));
+    validateClapCookie(checkClapCookie(id));
 
-        clap.click(function() {
-            if (checkClapCookie(id) != 1) {
-                getPostData(id, url).done(function(data) {
-                    var likeCount = data.acf.likes;
-                    addLike(likeCount, id, url);
-                    $(this).addClass('liked');
-                });
-            } else {
-                return false;
-            }
-        });
+    clap.click(function() {
+        if (checkClapCookie(id) != 1) {
+            getPostData(id, url).done(function(data) {
+                var likeCount = data.acf.likes;
+                addLike(likeCount, id, url);
+                $(this).addClass('liked');
+            });
+        } else {
+            return false;
+        }
+    });
 
-    };
+};
 
-});
 
 module.exports = {
     likePost
