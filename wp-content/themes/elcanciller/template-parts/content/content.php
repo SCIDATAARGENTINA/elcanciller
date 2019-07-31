f7caa7c36161854e7722ac22e27a45db7a89da90
Ian Lucas Diaz <ianlucasdiaz95@gmail.com>
2019-07-31 16:57:25 -0300

    asdasdasd

--- wp-content/themes/elcanciller/template-parts/content/content.php
+++ wp-content/themes/elcanciller/template-parts/content/content.php
@@ -42,7 +42,7 @@ if(get_field('show_author') == 'si'){
 				<a href="<?php get_term_link($term); ?>"><h4><?php echo $term->name ?></h4></a>
 			</div>
 		</div><!-- hovered -->
-		<div class="render-author" style="background-color: <?php $cat_color ?>">
+		<div class="render-author" style="background-color: <?php echo $cat_color ?>">
 			<span>Por: <?php echo get_the_author_meta( 'display_name' ) ?></span>
 		</div><!-- render author -->
 	</div><!-- rendered-img -->
