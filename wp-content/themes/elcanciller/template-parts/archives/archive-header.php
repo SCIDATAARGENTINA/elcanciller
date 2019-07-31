4f8e2335c368a9c1ea9d1c33edccc9abece37816
Ian Lucas Diaz <ianlucasdiaz95@gmail.com>
2019-07-31 17:39:17 -0300

    asdasd

--- wp-content/themes/elcanciller/template-parts/archives/archive-header.php
+++ wp-content/themes/elcanciller/template-parts/archives/archive-header.php
@@ -46,23 +46,13 @@ $term = get_queried_object();
          $alt = get_post_meta($thumbnail_id, '_wp_attachment_image_alt', true);
          $categories = get_the_category($post->ID);
          $cat_link = get_term_link($categories[0]->term_id );
+         $cat_color = get_field('color', $categories[0]->taxonomy . '_' . $categories[0]->term_id);
          ?>
 
-         <div class="titular">
-            <div class="category">
-               <a href="<?php echo $cat_link; ?>"><h3 class="category-name"><?php echo $categories[0]->name ?></h3></a>
-               <span><img src="<?php bloginfo('url') ?>/wp-content/uploads/2019/07/fire-marron.svg" alt=""></span>
-            </div><!-- category -->
-            <a href="<?php the_permalink(); ?>"><h2><?php the_title(); ?></h2></a>
-         </div><!-- titulo -->
-         
-
-         <div class="entry">
-            <div class="entry-img">
-               <a href="<?php the_permalink(); ?>"><img src="<?php echo $featured_img_url ?>" alt="<?php echo $alt ?>"></a>
-            </div>
-            <?php get_template_part('template-parts/comments/comments', 'sharer') ?>
-         </div>
+         <header class="archive-header" style="background: <?php echo $cat_color ?>">
+            <h1><?php echo $categories[0]->name ?></h1>
+            <h2><?php the_title(); ?></h2>
+         </header>
       <?php endwhile ?>
 
    <?php endif ?>
