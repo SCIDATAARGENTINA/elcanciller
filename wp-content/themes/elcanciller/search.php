b30655e86c8ebab197e935270367b201f2d2ff1b
Ian Lucas Diaz <ianlucasdiaz95@gmail.com>
2019-08-05 12:14:49 -0300

    asdasd

--- wp-content/themes/elcanciller/search.php
+++ wp-content/themes/elcanciller/search.php
@@ -45,7 +45,6 @@ get_header();
 				endwhile;
 
 
-				global $wp_query; // you can remove this line if everything works for you
  
 				// don't display the button if there are not enough posts
 				if (  $wp_query->max_num_pages > 1 ){
