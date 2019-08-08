a94219ec7875ad8f1d7afb46a1630ac2893540f9
Ian Lucas Diaz <ianlucasdiaz95@gmail.com>
2019-08-08 12:17:33 -0300

    asdasdasd

--- wp-content/themes/elcanciller/functions.php
+++ wp-content/themes/elcanciller/functions.php
@@ -547,11 +547,10 @@ function loadmore_ajax_handler(){
  
 			// look into your theme code how the posts are inserted, but you can use your own HTML of course
       // do you remember? - my example is adapted for Twenty Seventeen theme
-      if(is_search(  )){
+      if(is_search()){
         get_template_part( 'template-parts/content/content', 'search' );
       }else{
 				get_template_part( 'template-parts/content/content' );
-
       }
 			// for the test purposes comment the line above and uncomment the below one
 			// the_title();
