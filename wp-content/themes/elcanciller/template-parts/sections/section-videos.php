83ab2663b59813347769dedefd5af9492d44177d
Ian Lucas Diaz <ianlucasdiaz95@gmail.com>
2019-08-15 12:10:41 -0300

    asdasd

--- wp-content/themes/elcanciller/template-parts/sections/section-videos.php
+++ wp-content/themes/elcanciller/template-parts/sections/section-videos.php
@@ -33,8 +33,22 @@
                     while( $videolist->have_posts() ) {
                         $videolist->the_post(); ?>
                         
-                        <div class="video-item">
+                        <div id="video-<?php echo $post->ID ?>" data-id="<?php echo $post->ID ?>" class="video-item popup-video">
                             <h3><?php the_title(); ?></h3>
+                        </div>
+                        <div id="video-popup-<?php echo $post->ID ?>" class="player-content mfp-hide">
+                            <div class="popup-content">
+                                <div class="videos-title">
+                                    <div class="videos-pic">
+                                        <a href="#"><img src="<?php bloginfo('url') ?>/wp-content/uploads/2019/07/videos-logo.svg" alt="El Canciller - Videos"></a>
+                                    </div><!-- end videos-pic -->
+                                    <div class="videos-data">
+                                        <h4><a href="<?php echo $term_link ?>"><?php echo $categories[0]->name ?></a></h4>
+                                        <h3><?php the_title(); ?></h3>
+                                    </div><!-- end videos-data -->
+                                </div><!-- end videos-title -->
+                                <?php echo get_the_content(); ?>
+                            </div>
                         </div>
                         
                     <?php }
