cafadb16e612eeefeaeb86eb97db1cbee3e1b0f8
Ian Lucas Diaz <ianlucasdiaz95@gmail.com>
2019-08-15 14:27:45 -0300

    asdasddfasda

--- wp-content/themes/elcanciller/functions.php
+++ wp-content/themes/elcanciller/functions.php
@@ -280,6 +280,67 @@ function custom_post_type_placa()
 }
 add_action('init', 'custom_post_type_placa', 0);
 
+
+// Register Custom Post Type
+function custom_post_type_encuesta()
+{
+
+  $labels = array(
+    'name'                  => _x('Encuestas', 'Post Type General Name', 'elcanciller'),
+    'singular_name'         => _x('Encuesta', 'Post Type Singular Name', 'elcanciller'),
+    'menu_name'             => __('Encuestas', 'elcanciller'),
+    'name_admin_bar'        => __('Encuesta', 'elcanciller'),
+    'archives'              => __('Archivo', 'elcanciller'),
+    'attributes'            => __('Atributos', 'elcanciller'),
+    'parent_item_colon'     => __('Padre:', 'elcanciller'),
+    'all_items'             => __('Todos los items', 'elcanciller'),
+    'add_new_item'          => __('Agregar nuevo', 'elcanciller'),
+    'add_new'               => __('Agregar nuevo', 'elcanciller'),
+    'new_item'              => __('Nuevo', 'elcanciller'),
+    'edit_item'             => __('Editar', 'elcanciller'),
+    'update_item'           => __('Actualizar', 'elcanciller'),
+    'view_item'             => __('Ver', 'elcanciller'),
+    'view_items'            => __('Ver', 'elcanciller'),
+    'search_items'          => __('Buscar', 'elcanciller'),
+    'not_found'             => __('No encontrado', 'elcanciller'),
+    'not_found_in_trash'    => __('No encontrado en la papelera', 'elcanciller'),
+    'featured_image'        => __('Imagen destacada', 'elcanciller'),
+    'set_featured_image'    => __('Agregar imagen destacada', 'elcanciller'),
+    'remove_featured_image' => __('Borrar imagen destacada', 'elcanciller'),
+    'use_featured_image'    => __('Usar como imagen destacada', 'elcanciller'),
+    'insert_into_item'      => __('Insertar', 'elcanciller'),
+    'uploaded_to_this_item' => __('Cargado', 'elcanciller'),
+    'items_list'            => __('Listado', 'elcanciller'),
+    'items_list_navigation' => __('Navegacion del listado', 'elcanciller'),
+    'filter_items_list'     => __('Filtrar listado', 'elcanciller'),
+  );
+  $args = array(
+    'label'                 => __('Encuestas', 'elcanciller'),
+    'description'           => __('Modulo de administración de Encuestas', 'elcanciller'),
+    'labels'                => $labels,
+    'supports'              => array('title', 'editor', 'author', 'thumbnail'),
+    'taxonomies'            => array(),
+    'hierarchical'          => false,
+    'public'                => true,
+    'show_ui'               => true,
+    'show_in_menu'          => true,
+    'menu_icon'             => 'dashicons-welcome-widgets-menus',
+    'menu_position'         => 5,
+    'show_in_admin_bar'     => false,
+    'show_in_nav_menus'     => false,
+    'can_export'            => true,
+    'has_archive'           => false,
+    'exclude_from_search'   => true,
+    'publicly_queryable'    => true,
+    'show_in_rest'          => true,
+    'rest_base'             => 'encuestas',
+    'rest_controller_class' => 'WP_REST_Posts_Controller',
+    'capability_type'       => 'post',
+  );
+  register_post_type('encuesta', $args);
+}
+add_action('init', 'custom_post_type_encuesta', 0);
+
 //Crear taxonomia personalizada para videos.
 add_action( 'init', 'cat_videos_custom_taxonomy', 0 );
  
