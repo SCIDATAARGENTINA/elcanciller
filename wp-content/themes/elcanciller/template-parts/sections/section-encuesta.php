e0928bbacf8eb6763da50c2e6dae56a341ac0875
Ian Lucas Diaz <ianlucasdiaz95@gmail.com>
2019-08-16 12:26:32 -0300

    asdasd

--- wp-content/themes/elcanciller/template-parts/sections/section-encuesta.php
+++ wp-content/themes/elcanciller/template-parts/sections/section-encuesta.php
@@ -22,7 +22,7 @@
                     $query = new WP_Query( $args );
                     while( $query->have_posts() ) {
                         $query->the_post(); 
-                        echo get_field('encuesta');
+                        the_field('encuesta');
                         
                         get_template_part('template-parts/content/content', 'encuesta');
                     }
