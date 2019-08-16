571824b2353c8b814d07c5c89aeb29a5deecb574
Ian Lucas Diaz <ianlucasdiaz95@gmail.com>
2019-08-16 15:32:51 -0300

    asdasd

--- wp-content/themes/elcanciller/js/encuestas.js
+++ wp-content/themes/elcanciller/js/encuestas.js
@@ -1,14 +1,16 @@
 jQuery(document).ready(function($) {
 
-    var totVotos,
+    var idEncuesta,
+        totVotos,
         opcVotos,
         nOpcion;
 
-    let createOpcData = (totVotos, opcVotos, nOpcion) => {
+    let createOpcData = (idEncuesta, totVotos, opcVotos, nOpcion) => {
 
         let percentVotos = (opcVotos * 100) / totVotos;
 
         let data = {
+            idEncuesta,
             totVotos,
             opcVotos,
             nOpcion,
@@ -20,11 +22,14 @@ jQuery(document).ready(function($) {
     };
 
     $('.opcion').click(function() {
+        idEncuesta = $(this).attr('data-id');
         totVotos = $(this).attr('data-votos_totales');
         opcVotos = $(this).attr('data-votos');
         nOpcion = $(this).attr('data-row_index');
 
-        console.log(createOpcData(totVotos, opcVotos, nOpcion));
+        let data = createOpcData(idEncuesta, totVotos, opcVotos, nOpcion);
+
+        console.log(data);
     });
 
 
