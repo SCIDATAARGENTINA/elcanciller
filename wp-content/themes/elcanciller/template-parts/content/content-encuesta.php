52b842588c1760e1d1368a8f5028fa8b9e759412
Ian Lucas Diaz <ianlucasdiaz95@gmail.com>
2019-08-16 14:26:42 -0300

    asdasd

--- wp-content/themes/elcanciller/template-parts/content/content-encuesta.php
+++ wp-content/themes/elcanciller/template-parts/content/content-encuesta.php
@@ -17,7 +17,7 @@ $thumbnail_id = get_post_thumbnail_id($post->ID);
 $alt = get_post_meta($thumbnail_id, '_wp_attachment_image_alt', true);
 ?>
 
-<div id="encuesta-<?php echo $post->ID ?>" class="encuesta">
+<div id="encuesta-<?php echo $post->ID ?>" class="encuesta" data-votos="<?php get_field('total_votos', $post->ID) ?>">
     <div class="encuesta-container">
         <div class="encabezado">
             <img src="<?php bloginfo('url') ?>/wp-content/uploads/2019/08/encuesta-logo.svg" alt="">
@@ -35,8 +35,12 @@ $alt = get_post_meta($thumbnail_id, '_wp_attachment_image_alt', true);
                 while ( have_rows('opciones') ) : the_row(); 
                 $opcion = get_sub_field('opcion')
                 ?>
+
+                <pre>
+                    <?php print_r($opcion); ?>
+                </pre>
                 
-                    <div class="opcion">
+                    <div class="opcion" data-votos="<?php $opcion['votos'] ?>">
                         <div class="image-container" style="background-image: url('<?php echo wp_get_attachment_url( $opcion['imagen'] ) ?> ?>')">
                         </div>
                         <div class="titulo-opcion">
