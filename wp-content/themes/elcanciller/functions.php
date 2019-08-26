05ca91efd2e69db5f5f86d2e9da4ea0d4e16158a
Ian Lucas Diaz <ianlucasdiaz95@gmail.com>
2019-08-26 11:29:47 -0300

    asdasd

--- wp-content/themes/elcanciller/functions.php
+++ wp-content/themes/elcanciller/functions.php
@@ -655,7 +655,7 @@ function encuestas_ajax_handler(){
   $votos_opcion++;
   $votos_totales++;
 
-  update_field('field_5d559ccb6ab24', $votos_opcion, $id_encuesta); //update votos de la opcion
+  update_row('field_5d559ccb6ab24', $numero_opcion, $votos_opcion, $id_encuesta); //Update datos opcion
   update_field('field_5d559d1f6ab25', $votos_totales, $id_encuesta); // update votos totales de la encuesta
 
 }
