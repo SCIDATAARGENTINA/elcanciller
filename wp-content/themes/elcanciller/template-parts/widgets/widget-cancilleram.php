877b9b82521f577b31b700a7949f162b41f2b4ea
Ian Lucas Diaz <ianlucasdiaz95@gmail.com>
2019-08-29 18:04:20 -0300

    asdasd

--- wp-content/themes/elcanciller/template-parts/widgets/widget-cancilleram.php
+++ wp-content/themes/elcanciller/template-parts/widgets/widget-cancilleram.php
@@ -56,6 +56,7 @@
 
    <?php endif ?>
    </div>
+   <hr class="yellow-hr">
    <!-- Begin Mailchimp Signup Form -->
     <link href="//cdn-images.mailchimp.com/embedcode/horizontal-slim-10_7.css" rel="stylesheet" type="text/css">
     <style type="text/css">
