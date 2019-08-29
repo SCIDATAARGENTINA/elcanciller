fd53286d5356ae265388f23eeaf7d8d4f3ed6cbd
Ian Lucas Diaz <ianlucasdiaz95@gmail.com>
2019-08-29 14:30:08 -0300

    asdasd

--- wp-content/themes/elcanciller/header.php
+++ wp-content/themes/elcanciller/header.php
@@ -16,7 +16,7 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
 	<link rel="profile" href="https://gmpg.org/xfn/11" />
-	<link href="https://fonts.googleapis.com/css?family=Bree+Serif|Playfair+Display:400,400i,700,700i" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Bree+Serif|Playfair+Display:400,400i,700,700i|Lato:300,400,400i,700&display=swap" rel="stylesheet">
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
 	<?php wp_head(); ?>
 </head>
