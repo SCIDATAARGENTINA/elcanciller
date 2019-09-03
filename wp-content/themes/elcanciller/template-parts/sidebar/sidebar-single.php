43a299ec42173558f3e17bf9cfc877750382b989
Lucas <lucas@scidata.com.ar>
2019-09-03 18:49:10 -0300

    2L

--- wp-content/themes/elcanciller/template-parts/sidebar/sidebar-single.php
+++ wp-content/themes/elcanciller/template-parts/sidebar/sidebar-single.php
@@ -15,9 +15,10 @@
   <img src="<?php echo get_stylesheet_directory_uri() ?>/icons/c-stories.svg" alt="Stories - El Canciller">
 </div><!-- stories -->
 <div class="social-links">
-  <a href="#"><i class="fab fa-facebook-square"></i></a>
-  <a href="#"><i class="fab fa-twitter-square"></i></a>
-  <a href="#"><i class="fab fa-vimeo-square"></i></a>
+<a href="https://www.facebook.com/elcancillercom/" target="_blank" rel="noopener noreferrer"><i class="fab fa-facebook-square"></i></a>
+  <a href="https://twitter.com/elcancillercom" target="_blank" rel="noopener noreferrer"><i class="fab fa-twitter-square"></i></a>
+  <a href="https://www.instagram.com/elcancillerlive/" target="_blank" rel="noopener noreferrer"><i class="fab fa-instagram"></i></a>
+  <a href="https://www.youtube.com/channel/UCd9aVDXf_SH8-TNHRWj0J0g" target="_blank" rel="noopener noreferrer"><i class="fa fa-youtube-square"></i></a>
 </div><!-- social-links -->
 <div class="tiempo">
   <div class="canva-tiempo">
