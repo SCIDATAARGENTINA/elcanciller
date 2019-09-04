457e766eda8e40fd33ea791586a9414f43f3186d
Ian Lucas Diaz <ianlucasdiaz95@gmail.com>
2019-09-04 18:17:49 -0300

    asdasdASDASD

--- wp-content/themes/elcanciller/functions.php
+++ wp-content/themes/elcanciller/functions.php
@@ -725,3 +725,31 @@ function mostrar_posts($atts){
 }
 
 add_shortcode( 'posts', 'mostrar_posts' );
+
+/*ACF READONLY*/
+
+add_action('acf/render_field_settings/type=text', 'add_readonly_and_disabled_to_text_field');
+  function add_readonly_and_disabled_to_text_field($field) {
+    acf_render_field_setting( $field, array(
+      'label'      => __('Read Only?','acf'),
+      'instructions'  => '',
+      'type'      => 'radio',
+      'name'      => 'readonly',
+      'choices'    => array(
+        1        => __("Yes",'acf'),
+        0        => __("No",'acf'),
+      ),
+      'layout'  =>  'horizontal',
+    ));
+    acf_render_field_setting( $field, array(
+      'label'      => __('Disabled?','acf'),
+      'instructions'  => '',
+      'type'      => 'radio',
+      'name'      => 'disabled',
+      'choices'    => array(
+        1        => __("Yes",'acf'),
+        0        => __("No",'acf'),
+      ),
+      'layout'  =>  'horizontal',
+    ));
+  }
