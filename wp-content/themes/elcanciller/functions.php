74e878f9c6fa6e0d8d338201d493ebdf42aca831
Ian Lucas Diaz <ianlucasdiaz95@gmail.com>
2019-09-05 14:31:47 -0300

    asdasd

--- wp-content/themes/elcanciller/functions.php
+++ wp-content/themes/elcanciller/functions.php
@@ -629,15 +629,6 @@ function post_recomendado($atts){
 
 add_shortcode( 'recomendado', 'post_recomendado' );
 
-function wp_api_encode_acf($data,$post,$context){
-	$data['meta'] = array_merge($data['meta'],get_fields($post['ID']));
-	return $data;
-}
-
-if( function_exists('get_fields') ){
-	add_filter('json_prepare_post', 'wp_api_encode_acf', 10, 3);
-}
-
 // AJAX LOAD MORE
 
 function load_more_scripts() {
