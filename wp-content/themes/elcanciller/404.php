e15d9e347e182086d76d417199caeeb5f9416d7c
Lucas <lucas@scidata.com.ar>
2019-09-04 23:26:18 -0300

    20

--- wp-content/themes/elcanciller/404.php
+++ wp-content/themes/elcanciller/404.php
@@ -16,15 +16,16 @@ get_header();
 		<main id="main" class="site-main">
 
 			<div class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php _e( 'Oops! That page can&rsquo;t be found.', 'twentynineteen' ); ?></h1>
-				</header><!-- .page-header -->
+				<img class="error-image" src="http://142.93.24.13/wp-content/uploads/2019/09/error-404.png">
+				<!-- <header class="page-header">
+					<h1 class="page-title"><?php //_e( 'Oops! That page can&rsquo;t be found.', 'twentynineteen' ); ?></h1>
+				</header>-->
 
-				<div class="page-content">
-					<p><?php _e( 'It looks like nothing was found at this location. Maybe try a search?Lucas', 'twentynineteen' ); ?></p>
-					<?php get_search_form(); ?>
-				</div><!-- .page-content -->
-			</div><!-- .error-404 -->
+				<!-- <div class="page-content">
+					<p>--><?php //_e( 'It looks like nothing was found at this location. Maybe try a search?', 'twentynineteen' ); ?></p>
+					<?php //get_search_form(); ?>
+				<!--</div>-->
+			</div>
 
 		</main><!-- #main -->
 	</section><!-- #primary -->
