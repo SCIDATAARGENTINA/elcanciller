25968792a96a1c13a3f1509b5a068f67564f6677
Lucas <lucas@scidata.com.ar>
2019-09-05 11:48:38 -0300

    22

--- wp-content/themes/elcanciller/template-parts/content/content-contador.php
+++ wp-content/themes/elcanciller/template-parts/content/content-contador.php
@@ -16,6 +16,6 @@ $alt = get_post_meta($thumbnail_id, '_wp_attachment_image_alt', true);
 
 <article id="contador-<?php the_ID(); ?>">
 <h3> <?php the_title()?> </h3>
-<p> <?php the_field('fecha')?> </p>
+<p> <?php the_field('fecha_limite')?> </p>
 <p> <?php the_field('cuerpo')?> </p>
 </article><!-- #post-${ID} -->
