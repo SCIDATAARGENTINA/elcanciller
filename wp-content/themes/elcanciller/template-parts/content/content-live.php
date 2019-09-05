5a6267c2b1d23f8fb73855efd42a80f0183475ff
Lucas <lucas@scidata.com.ar>
2019-09-05 17:38:07 -0300

    123124

--- wp-content/themes/elcanciller/template-parts/content/content-live.php
+++ wp-content/themes/elcanciller/template-parts/content/content-live.php
@@ -14,7 +14,7 @@ $thumbnail_id = get_post_thumbnail_id($post->ID);
 $alt = get_post_meta($thumbnail_id, '_wp_attachment_image_alt', true);
 ?>
 
-<?php if(get_field('habilitado') != 'si'){ ?>
+<?php if(get_field('habilitado') != 'no'){ ?>
 <article id="live-<?php the_ID(); ?>">
 <p><?php the_field('codigo_embebido'); ?> </p>
 </article><!-- #post-${ID} -->
