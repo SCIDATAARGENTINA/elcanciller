bcaebba8da2730596e9630be8ce5bd4e6926f600
Ian Lucas Diaz <ianlucasdiaz95@gmail.com>
2019-09-08 03:33:17 -0300

    asdasd

--- wp-content/themes/elcanciller/template-parts/content/content-contador.php
+++ wp-content/themes/elcanciller/template-parts/content/content-contador.php
@@ -12,6 +12,15 @@
 $featured_img_url = get_the_post_thumbnail_url(get_the_ID(), 'full');
 $thumbnail_id = get_post_thumbnail_id($post->ID);
 $alt = get_post_meta($thumbnail_id, '_wp_attachment_image_alt', true);
+
+$date = get_field('fecha_limite');
+
+$date = new DateTime($date);
+
+$interval = $future_date->diff($now);
+
+echo $interval->format("a");
+
 ?>
 
 <article id="contador-<?php the_ID(); ?>">
