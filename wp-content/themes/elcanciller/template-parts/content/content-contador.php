4e5d45f8e545dee326c29f643b86dcfcf6bb925f
Lucas <lucas@scidata.com.ar>
2019-09-09 20:19:53 -0300

    221

--- wp-content/themes/elcanciller/template-parts/content/content-contador.php
+++ wp-content/themes/elcanciller/template-parts/content/content-contador.php
@@ -29,7 +29,7 @@ $intervalo = $fecha_limite->diff($fecha_hoy);
         <h3> <?php the_title()?> </h3> <span class="contador-header-subtitulo">Faltan_</span>
     </div>
     <p class="contador-cuerpo" style="background-image: url('<?php the_field('imagen_de_fondo') ?>');background-size: cover;background-repeat: no-repeat;"> 
-        <?php echo $fecha_limite; ?> 
+        <span class="contador-numero">84</span><span class="contador-rango">DÍAS</span>
     </p>
     <p class="contador-footer"> <?php the_field('cuerpo')?> </p>
 </article><!-- #post-${ID} -->
