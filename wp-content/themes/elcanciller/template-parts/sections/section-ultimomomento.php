02377814f0e6eefa07cd37ea419726fca4749b4d
Lucas <lucas@scidata.com.ar>
2019-09-09 13:43:54 -0300

    5511111

--- wp-content/themes/elcanciller/template-parts/sections/section-ultimomomento.php
+++ wp-content/themes/elcanciller/template-parts/sections/section-ultimomomento.php
@@ -35,9 +35,9 @@ $alt = get_post_meta($thumbnail_id, '_wp_attachment_image_alt', true);
          <div class="trending-post">
           <div class="titular">
               <div class="category">
-                <span style=" display: table-cell;vertical-align: middle;"><h3 class="category-name ultimoicon">ULTIMO MOMENTO</h3></span>
+                <span style=" display: table-cell;vertical-align: middle;"><h3 class="category-name ultimoicon"><?php the_field('titular') ?></h3></span>
               </div><!-- category -->
-              <a href='"<?php the_field('url_embebido') ?>"' style="width: 60%;text-align: center;clear: both;margin: 1rem 0;"><h2><?php the_field('texto_embebido') ?></h2></a>
+              <a href='"<?php the_field('url_embebido') ?>"' style="text-align: center;clear: both;margin: 1rem 0;"><h2><?php the_field('texto_embebido') ?></h2></a>
           </div>
         </div>
 
