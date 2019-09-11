2f18c32e227c38432c5c4742a7da4d1d177d0c1f
Lucas <lucas@scidata.com.ar>
2019-09-11 16:56:53 -0300

    3

--- wp-content/themes/elcanciller/template-parts/content/content-contador.php
+++ wp-content/themes/elcanciller/template-parts/content/content-contador.php
@@ -29,7 +29,7 @@ $fecha2= $fecha_limite->createFromFormat('Y/m/d', get_field('fecha_limite'));
 
 $fechaq01 = get_field('fecha_limite');
 $fechaq1 = new DateTime($fechaq01);
-$fechaq2 = new DateTime(date("d/m/Y"));
+$fechaq2 = new DateTime(date("Y/m/d"));
 $resultado = $fechaq1->diff($fechaq2);
 echo $resultado->format('%R%a días');
  
