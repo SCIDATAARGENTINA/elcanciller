6d47c33f4398989cc25e17572d641cf35d059b25
Lucas <lucas@scidata.com.ar>
2019-09-11 16:33:53 -0300

    5ty6

--- wp-content/themes/elcanciller/template-parts/content/content-contador.php
+++ wp-content/themes/elcanciller/template-parts/content/content-contador.php
@@ -25,6 +25,7 @@ $fecha_hoy = new DateTime();
 
 //$fecha2= $fecha_limite->createFromFormat('d/m/Y', get_field('fecha_limite'));
 $fecha2= get_field('fecha_limite');
+$fecha02 = date("Y/m/d", strtotime($fecha2));
 //$fecha1= new DateTime(date("d/m/Y"));
 $fecha01 = date('Y/m/d');
 $diff = $fecha01->diff($fecha2);
