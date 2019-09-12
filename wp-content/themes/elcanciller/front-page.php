5b11fef8b979195d734072b230736808b7e6339c
Lucas <lucas@scidata.com.ar>
2019-09-12 15:59:50 -0300

    dddf

--- wp-content/themes/elcanciller/front-page.php
+++ wp-content/themes/elcanciller/front-page.php
@@ -12,6 +12,11 @@
 get_header();
 $page_id  = get_queried_object_id();
 ?>
+<script>
+	setTimeout(function() {
+   		window.location.reload;
+	}, 5000);
+</script>
 
 <section id="primary" class="content-area">
 	<main id="main" class="site-main">
--- wp-content/themes/elcanciller/header.php
+++ wp-content/themes/elcanciller/header.php
@@ -19,7 +19,6 @@
 	<link href="https://fonts.googleapis.com/css?family=Bree+Serif|Playfair+Display:400,400i,700,700i|Lato:300,400,400i,700&display=swap" rel="stylesheet">
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
 	<?php wp_head(); ?>
-	<meta http-equiv="refresh" content="180">
 </head>
 
 <body <?php body_class(); ?>>
