f62dbb04998595474d69eb27a829d2d5c4f21ba1
Ian Lucas Diaz <ianlucasdiaz95@gmail.com>
2019-09-12 15:26:21 -0300

    asdasd

--- wp-content/themes/elcanciller/template-parts/author/author-header.php
+++ wp-content/themes/elcanciller/template-parts/author/author-header.php
@@ -12,7 +12,7 @@ $author = get_queried_object();
 $author_id = $author->ID;
 echo $author_id;
 $featured_img_url = get_the_post_thumbnail_url(get_field('portada','user_42'), 'full');
-$thumbnail_id = get_field('portada','42');
+$thumbnail_id = get_field('portada','user_42');
 $alt = get_post_meta($thumbnail_id, '_wp_attachment_image_alt', true);
 ?>
 
