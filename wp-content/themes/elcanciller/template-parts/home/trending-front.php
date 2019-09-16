fbd81bb76b75821935c3873571ca0171251f13eb
Ian Lucas Diaz <ianlucasdiaz95@gmail.com>
2019-09-16 16:24:43 -0300

    asdasd

--- wp-content/themes/elcanciller/template-parts/home/trending-front.php
+++ wp-content/themes/elcanciller/template-parts/home/trending-front.php
@@ -33,29 +33,8 @@
 
       <?php while ($trending_post->have_posts()) : $trending_post->the_post() ?>
 
-         <?php
-         $featured_img_url = get_the_post_thumbnail_url(get_the_ID(), 'full');
-         $thumbnail_id = get_post_thumbnail_id($post->ID);
-         $alt = get_post_meta($thumbnail_id, '_wp_attachment_image_alt', true);
-         $categories = get_the_category($post->ID);
-         $cat_link = get_term_link($categories[0]->term_id );
-         ?>
+      <?php get_template_part('template-parts/home/trending-front', 'content') ?>
 
-         <div class="titular">
-            <div class="category">
-               <a href="<?php echo $cat_link; ?>"><h3 class="category-name"><?php echo $categories[0]->name ?></h3></a>
-               <span><img src="<?php bloginfo('url') ?>/wp-content/uploads/2019/07/fire-marron.svg" alt=""></span>
-            </div><!-- category -->
-            <a href="<?php the_permalink(); ?>"><h2><?php the_title(); ?></h2></a>
-         </div><!-- titulo -->
-         
-
-         <div class="entry">
-            <div class="entry-img">
-               <a href="<?php the_permalink(); ?>"><img src="<?php echo $featured_img_url ?>" alt="<?php echo $alt ?>"></a>
-            </div>
-            <?php get_template_part('template-parts/comments/comments', 'sharer') ?>
-         </div>
       <?php endwhile ?>
 
    <?php endif ?>
