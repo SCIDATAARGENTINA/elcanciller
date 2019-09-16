f198a0e91ba683046d5e10bfc5b5ad00489121d8
Lucas <lucas@scidata.com.ar>
2019-09-16 15:59:30 -0300

    story

--- wp-content/themes/elcanciller/functions.php
+++ wp-content/themes/elcanciller/functions.php
@@ -529,6 +529,69 @@ add_action('init', 'custom_post_type_ultimomomento', 0);
 
 
 
+function custom_post_type_story()
+{
+
+  $labels = array(
+    'name'                  => _x('Ultimas Story', 'Post Type General Name', 'elcanciller'),
+    'singular_name'         => _x('Ultima Story', 'Post Type Singular Name', 'elcanciller'),
+    'menu_name'             => __('Ultima Story', 'elcanciller'),
+    'name_admin_bar'        => __('Ultimo Story', 'elcanciller'),
+    'archives'              => __('Archivo', 'elcanciller'),
+    'attributes'            => __('Atributos', 'elcanciller'),
+    'parent_item_colon'     => __('Padre:', 'elcanciller'),
+    'all_items'             => __('Todos los items', 'elcanciller'),
+    'add_new_item'          => __('Agregar nuevo', 'elcanciller'),
+    'add_new'               => __('Agregar nuevo', 'elcanciller'),
+    'new_item'              => __('Nuevo', 'elcanciller'),
+    'edit_item'             => __('Editar', 'elcanciller'),
+    'update_item'           => __('Actualizar', 'elcanciller'),
+    'view_item'             => __('Ver', 'elcanciller'),
+    'view_items'            => __('Ver', 'elcanciller'),
+    'search_items'          => __('Buscar', 'elcanciller'),
+    'not_found'             => __('No encontrado', 'elcanciller'),
+    'not_found_in_trash'    => __('No encontrado en la papelera', 'elcanciller'),
+    'featured_image'        => __('Imagen destacada', 'elcanciller'),
+    'set_featured_image'    => __('Agregar imagen destacada', 'elcanciller'),
+    'remove_featured_image' => __('Borrar imagen destacada', 'elcanciller'),
+    'use_featured_image'    => __('Usar como imagen destacada', 'elcanciller'),
+    'insert_into_item'      => __('Insertar', 'elcanciller'),
+    'uploaded_to_this_item' => __('Cargado', 'elcanciller'),
+    'items_list'            => __('Listado', 'elcanciller'),
+    'items_list_navigation' => __('Navegacion del listado', 'elcanciller'),
+    'filter_items_list'     => __('Filtrar listado', 'elcanciller'),
+  );
+  $args = array(
+    'label'                 => __('Story', 'elcanciller'),
+    'description'           => __('Modulo de administración de Story', 'elcanciller'),
+    'labels'                => $labels,
+    'supports'              => array('title'),
+    'taxonomies'            => array(),
+    'hierarchical'          => false,
+    'public'                => true,
+    'show_ui'               => true,
+    'show_in_menu'          => true,
+    'menu_position'         => 6,
+    'menu_icon'             => 'dashicons-images-alt2',
+    'show_in_admin_bar'     => false,
+    'show_in_nav_menus'     => false,
+    'can_export'            => true,
+    'has_archive'           => false,
+    'exclude_from_search'   => true,
+    'publicly_queryable'    => true,
+    'show_in_rest'          => true,
+    'rest_base'             => 'story',
+    'rest_controller_class' => 'WP_REST_Posts_Controller',
+    'capability_type'       => 'post',
+  );
+  register_post_type('story', $args);
+}
+add_action('init', 'custom_post_type_story', 0);
+
+
+
+
+
 //Crear taxonomia personalizada para videos.
 add_action( 'init', 'cat_videos_custom_taxonomy', 0 );
  
@@ -1076,3 +1139,33 @@ function ultimomomento_shortcode($atts){
 }
 
 add_shortcode( 'ultimomomento', 'ultimomomento_shortcode' );
+
+
+
+function story_shortcode($atts){
+  $a = story_atts( array(
+    'postid' => '0'
+  ), $atts );
+  
+    // Setup arguments.
+    $args = array(
+      'post_type' => 'story',
+      'posts_per_page' => 1,
+      'p' => $a['postid']
+  );
+  
+  $story_query = new WP_Query( $args ); 
+
+  if($story_query->have_posts()){
+    while($story_query->have_posts()){
+      $story_query->the_post();
+
+      get_template_part( 'template-parts/content/content', 'story' );
+
+    }
+  }
+  
+
+}
+
+add_shortcode( 'story', 'story_shortcode' );
