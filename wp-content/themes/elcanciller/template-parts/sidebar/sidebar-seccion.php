d3baab4b938a0c47827c8c24ce4d0c7acef0319b
Lucas <lucas@scidata.com.ar>
2019-09-16 12:13:28 -0300

    13

--- wp-content/themes/elcanciller/template-parts/sidebar/sidebar-seccion.php
+++ wp-content/themes/elcanciller/template-parts/sidebar/sidebar-seccion.php
@@ -40,3 +40,13 @@
 <div class="widget">
   <?php get_template_part('template-parts/widgets/widget', 'twitter') ?>
 </div>
+<div class="widget widget-contador">
+  <?php echo do_shortcode('[contador postid=48914]') ?>
+</div>
+<div class="widget">
+  <a href="https://www.gba.gob.ar/seguridad?utm_source=medios&utm_medium=cpc&utm_campaign=seguridad"><img src="https://elcanciller.com/files/juego.gif"></a>
+  <!-- /21749555895/Home-Lateral-SeccionEstaPasando-250x250 -->
+  <div id='div-gpt-ad-1539357193770-0' style='height:250px; width:250px;margin:0 auto;margin-left: 0px;'>
+    <script>googletag.cmd.push(function() { googletag.display('div-gpt-ad-1539357193770-0'); });</script>
+  </div>
+</div>
