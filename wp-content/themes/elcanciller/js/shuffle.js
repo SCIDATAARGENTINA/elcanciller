ed132069ebf7340f606cc46ba709cab45594ff55
Ian Lucas Diaz <ianlucasdiaz95@gmail.com>
2019-09-16 17:00:09 -0300

    asdasd

---
+++ wp-content/themes/elcanciller/js/shuffle.js
@@ -0,0 +1,22 @@
+jQuery(function ($) { // use jQuery code inside this to avoid "$ is not defined" error
+    $('.trending-post .shuffle').click(function () {
+        var trendingPost = $('.trending-post');
+        var button = $(this),
+            data = {
+                'action': 'shuffle',
+            };
+
+
+        $.ajax({ // you can also use $.post here
+            url: shuffle_params.ajaxurl, // AJAX handler
+            data: data,
+            type: 'POST',
+            beforeSend: function (xhr) {
+
+            },
+            success: function (data) {
+                console.log(data);
+            }
+        });
+    });
+});
