e9c432b3caaa54a3aff136adfad053e178928b17
Ian Lucas Diaz <ianlucasdiaz95@gmail.com>
2019-09-17 23:02:57 -0300

    asdasd

--- wp-content/themes/elcanciller/template-parts/content/content-historia.php
+++ wp-content/themes/elcanciller/template-parts/content/content-historia.php
@@ -8,14 +8,19 @@
  * @subpackage Twenty_Nineteen
  * @since 1.0.0
  */
-
-$featured_img_url = get_the_post_thumbnail_url(get_the_ID(), 'full');
-$thumbnail_id = get_post_thumbnail_id($post->ID);
+$thumbnail_id = get_field('imagen');
+$featured_img_url = wp_get_attachment_image_src( $thumbnail_id, 'full' );
 $alt = get_post_meta($thumbnail_id, '_wp_attachment_image_alt', true);
 ?>
 
-<?php if(get_field('habilitado') != 'no'){ ?>
-<article id="live-<?php the_ID(); ?>">
-<p><?php the_field('codigo_embebido'); ?> </p>
+<article id="historia-<?php the_ID(); ?>">
+
+    <h3><?php the_title(); ?></h3>
+
+    <img src="<?php echo $featured_img_url ?>" alt="<?php echo $alt ?>">
+
+    <?php if(get_field('link')){ ?>
+        <a href="<?php get_field('link') ?>">Ver más</a>
+    <?php } ?>
+
 </article><!-- #post-${ID} -->
-<?php } ?>
