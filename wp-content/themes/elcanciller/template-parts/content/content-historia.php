119c40710f8ae6a593b162d9f781d4c0470f3afd
Ian Lucas Diaz <ianlucasdiaz95@gmail.com>
2019-09-17 23:25:56 -0300

    asdasd

--- wp-content/themes/elcanciller/template-parts/content/content-historia.php
+++ wp-content/themes/elcanciller/template-parts/content/content-historia.php
@@ -9,7 +9,7 @@
  * @since 1.0.0
  */
 $thumbnail_id = get_field('imagen');
-$featured_img = wp_get_attachment_image_src( $thumbnail_id, 'full' );
+$featured_img = wp_get_attachment_image_src( $thumbnail_id, 'medium' );
 $alt = get_post_meta($thumbnail_id, '_wp_attachment_image_alt', true);
 ?>
 
