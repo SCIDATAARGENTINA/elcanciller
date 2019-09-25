97ce6e2a2ca599b4a97bc4c42a7e0413a2ea49b2
Ian Lucas Diaz <ianlucasdiaz95@gmail.com>
2019-09-25 11:36:03 -0300

    <zx

--- wp-content/themes/elcanciller/js/shuffle.js
+++ wp-content/themes/elcanciller/js/shuffle.js
@@ -1,4 +1,4 @@
-jQuery(function ($) { // use jQuery code inside this to avoid "$ is not defined" error
+jQuery(function ($) { 
     $(document).on('click', '.trending-post .shuffle', function () {
         var trendingPost = $('.trending-post');
         var button = $(this),
@@ -7,16 +7,16 @@ jQuery(function ($) { // use jQuery code inside this to avoid "$ is not defined"
             };
 
 
-        $.ajax({ // you can also use $.post here
-            url: shuffle_params.ajaxurl, // AJAX handler
+        $.ajax({ 
+            url: shuffle_params.ajaxurl,
             data: data,
             type: 'POST',
             beforeSend: function (xhr) {
-                //trendingPost.css('opacity', '0').css('transform','translateX(20px)');
+                trendingPost.css('opacity', '0').css('transform','translateX(20px)');
             },
             success: function (data) {
                 trendingPost.html(data);
-                //trendingPost.css('opacity', '1').css('transform', 'translateX(0px)');
+                trendingPost.css('opacity', '1').css('transform', 'translateX(0px)');
             },
             error: function (error) {
                 console.log(`Error ${error}`);
