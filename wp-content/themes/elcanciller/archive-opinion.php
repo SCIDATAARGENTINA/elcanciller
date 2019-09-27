ef53b362c5e67e7a40629e584e7646131051de79
Ian Lucas Diaz <ianlucasdiaz95@gmail.com>
2019-09-27 17:06:54 -0300

    asdasd

--- wp-content/themes/elcanciller/archive-opinion.php
+++ wp-content/themes/elcanciller/archive-opinion.php
@@ -39,7 +39,7 @@ echo '<style> .' . $term->slug . ':before'. '{ background: ' . $cat_color . '; }
 				 * If you want to override this in a child theme, then include a file
 				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
 				 */
-				get_template_part( 'template-parts/content/content' );
+				get_template_part( 'template-parts/content/content-opinion' );
 
 				// End the loop.
 			endwhile;
