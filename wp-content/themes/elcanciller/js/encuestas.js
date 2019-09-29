c0d9b7cf3e04780064226309df68d39adcc79e19
Lucas <lucas@scidata.com.ar>
2019-09-28 23:13:55 -0300

    encuestasjs

--- wp-content/themes/elcanciller/js/encuestas.js
+++ wp-content/themes/elcanciller/js/encuestas.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function($) {
     var URLdomain = 'http://'+ window.location.host;
-
+    
     var idEncuesta,
         totVotos,
         opcVotos,
@@ -10,7 +10,7 @@ jQuery(document).ready(function($) {
 
     let getEncuesta = (encuestaId) => {
 
-        return $.get(\'"+URLdomain+"/wp-json/wp/v2/encuestas/${encuestaId}`);
+        return $.get(`${URLdomain}/wp-json/wp/v2/encuestas/${encuestaId}`);
 
     };
 
