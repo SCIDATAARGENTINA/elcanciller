774a602781b9bf614e84e221bfba3cf3557aa511
Lucas <lucas@scidata.com.ar>
2019-09-30 17:54:25 -0300

    live 2

--- wp-content/themes/elcanciller/template-parts/sections/section-live.php
+++ wp-content/themes/elcanciller/template-parts/sections/section-live.php
@@ -32,7 +32,7 @@ $alt = get_post_meta($thumbnail_id, '_wp_attachment_image_alt', true);
             <a href="#"><h2 style="background-color: #fff;color:#ebd637;margin-top: 0%;padding: 2% 0% 2% 0%;">EN VIVO</h2></a>
          </div>
         <article id="live-<?php the_ID(); ?>" style="max-width: 100%;background-color: #fff;margin-top: -3%;">
-        <p style="margin-top: 0%;background-color: #000;padding: 2% 0% 2% 0%;border-bottom: 4px solid #e7d12d;"><?php the_field('codigo_embebido'); ?> </p>
+        <p style="margin-top: 0%;padding: 2% 0% 2% 0%;border-bottom: 4px solid #e7d12d;"><?php the_field('codigo_embebido'); ?> </p>
         </article><!-- #post-${ID} -->
         <?php }
 
