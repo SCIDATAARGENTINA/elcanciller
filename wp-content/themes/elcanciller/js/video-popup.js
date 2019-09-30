f4e191f0777c4622e97bbe31e61f7b3f12e146a9
Ian Lucas Diaz <ianlucasdiaz95@gmail.com>
2019-09-30 14:42:45 -0300

    asdasd

--- wp-content/themes/elcanciller/js/video-popup.js
+++ wp-content/themes/elcanciller/js/video-popup.js
@@ -7,8 +7,6 @@ jQuery(function ($) {
                 'id': id
             };
 
-        popupVideo.magnificPopup();
-
         $.ajax({ 
             url: video_params.ajaxurl,
             data: data,
@@ -23,15 +21,15 @@ jQuery(function ($) {
                     type: 'inline',
                     items: {
                         src: popupContent
-                    },
+                    }
                     // Delay in milliseconds before popup is removed
-                    removalDelay: 300,
+                    //removalDelay: 300,
 
                     // Class that is added to popup wrapper and background
                     // make it unique to apply your CSS animations just to this exact popup
-                    mainClass: 'mfp-fade',
-                    closeBtnInside: true,
-                    showCloseBtn: true
+                    //mainClass: 'mfp-fade',
+                    //closeBtnInside: true,
+                    //showCloseBtn: true
                 });
             },
             error: function (error) {
