f05a864c33149359fe363a0a32ea7b7127ecabf6
Ian Lucas Diaz <ianlucasdiaz95@gmail.com>
2019-10-03 12:21:07 -0300

    asdasd

--- wp-content/themes/elcanciller/functions.php
+++ wp-content/themes/elcanciller/functions.php
@@ -1035,7 +1035,7 @@ function mostrar_posts($atts){
       'post_type' => 'post',
       'posts_per_page' => $a['cantidad'],
       'offset' => $a['offset'],
-      'exclude' => $excluded
+      'exclude' => array($excluded)
   );
   
   $base_query = new WP_Query( $args ); 
