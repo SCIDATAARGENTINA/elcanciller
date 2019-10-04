2885d22ff826ee6be942618c76cf3a846a59a019
Ian Lucas Diaz <ianlucasdiaz95@gmail.com>
2019-10-04 15:22:54 -0300

    asdasd

--- wp-content/themes/elcanciller/front-page.php
+++ wp-content/themes/elcanciller/front-page.php
@@ -46,8 +46,10 @@ $encuesta_grande = get_field('encuesta_grande');
 				<?php 
 				if($encuesta_grande){
 					include( locate_template( 'template-parts/sections/section-encuesta.php', false, false ) ); 
+				}else if($encuesta_pequena){
+					echo do_shortcode('[posts cantidad="3" offset="2"]'); // Con encuesta pequeña sin encuesta grande - offset 2 total: 5
 				}else{
-					
+					echo do_shortcode('[posts cantidad="3" offset="3"]'); // Sin encuestas offset 3 total: 6
 				}
 				?>
 				<div class="col-3 order-2" data-quantity="2" data-offset="3">
@@ -55,10 +57,14 @@ $encuesta_grande = get_field('encuesta_grande');
 						<?php the_field('anuncio_1', $page_id) ?>
 					</div>
 					<?php 
-					if ($encuesta_pequena){
-						echo do_shortcode('[posts cantidad="2" offset="2"]');
-					}else{
-						echo do_shortcode('[posts cantidad="2" offset="3"]');
+					if ($encuesta_pequena && $encuesta_grande){
+						echo do_shortcode('[posts cantidad="2" offset="2"]'); // Con encuesta grande y pequeña offset: 2 total: 4
+					}else if($encuesta_pequena && !$encuesta_grande){ 
+						echo do_shortcode('[posts cantidad="2" offset="5"]'); // Con encuesta pequeña sin encuesta grande - offset 5 total: 7 
+					}else if(!$encuesta_pequena && $encuesta_grande){
+						echo do_shortcode('[posts cantidad="2" offset="3"]'); // Sin encuesta pequeña con encuesta grande - offset 3 total: 5
+					}else {
+						echo do_shortcode('[posts cantidad="2" offset="6"]'); // Sin encuestas - offset 6 total: 8
 					}
 					?>
 				</div>
@@ -81,10 +87,14 @@ $encuesta_grande = get_field('encuesta_grande');
 					<?php the_field('anuncio_6', $page_id) ?>
 				</div>
 				<?php
-				if ($encuesta_pequena){
-					echo do_shortcode('[posts cantidad="2" offset="4"]');
-				}else{
-					echo do_shortcode('[posts cantidad="2" offset="5"]');
+				if ($encuesta_pequena && $encuesta_grande){
+					echo do_shortcode('[posts cantidad="2" offset="4"]'); // Con encuesta grande y pequeña offset: 4 total: 6
+				}else if($encuesta_pequena && !$encuesta_grande){ 
+					echo do_shortcode('[posts cantidad="2" offset="7"]'); // Con encuesta pequeña sin encuesta grande - offset 7 total: 9 
+				}else if(!$encuesta_pequena && $encuesta_grande){
+					echo do_shortcode('[posts cantidad="2" offset="5"]'); // Sin encuesta pequeña con encuesta grande - offset 5 total: 7
+				}else {
+					echo do_shortcode('[posts cantidad="2" offset="8"]'); // Sin encuestas - offset 8 total: 10
 				}
 				?>
 			</div>
@@ -93,13 +103,16 @@ $encuesta_grande = get_field('encuesta_grande');
 					<?php the_field('anuncio_7', $page_id) ?>
 				</div>
 				<div class="col-2-2">
-				<?php 
-				if ($encuesta_pequena){
-					echo do_shortcode('[posts cantidad="4" offset="6"]');
-				}else{
-					echo do_shortcode('[posts cantidad="2" offset="7"]');
+				<?php
+				if ($encuesta_pequena && $encuesta_grande){
+					echo do_shortcode('[posts cantidad="4" offset="6"]'); // Con encuesta grande y pequeña offset: 6 total: 10
+				}else if($encuesta_pequena && !$encuesta_grande){ 
+					echo do_shortcode('[posts cantidad="4" offset="9"]'); // Con encuesta pequeña sin encuesta grande - offset 9 total: 13 
+				}else if(!$encuesta_pequena && $encuesta_grande){
+					echo do_shortcode('[posts cantidad="4" offset="7"]'); // Sin encuesta pequeña con encuesta grande - offset 7 total: 11
+				}else {
+					echo do_shortcode('[posts cantidad="4" offset="10"]'); // Sin encuestas - offset 10 total: 14
 				}
-					
 				?>
 				</div>				
 			</div>
