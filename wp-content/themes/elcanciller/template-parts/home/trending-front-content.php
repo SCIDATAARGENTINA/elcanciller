50ac633d9f1607ec3b0f1011cd6d3b3f88b72beb
Ian Lucas Diaz <ianlucasdiaz95@gmail.com>
2019-10-09 11:52:43 -0300

    asdasd

--- wp-content/themes/elcanciller/template-parts/home/trending-front-content.php
+++ wp-content/themes/elcanciller/template-parts/home/trending-front-content.php
@@ -14,9 +14,9 @@ $cat_link = get_term_link($categories[0]->term_id );
     <div class="titulo">
         <?php if(get_the_author_meta('ID') != 81 && get_field('show_author') == 'si'){ ?>
         <div class="post-author" >
-            <a style="background-color: #e7d117" href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' )) ?>">
+            <a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' )) ?>">
             <?php echo get_avatar( get_the_author_meta('ID'), 26 ); ?>
-            <span><?php echo get_the_author(); ?></span>
+            <span>Por: <?php echo get_the_author(); ?></span>
             </a>
         </div><!-- post-author -->
         <?php } ?>
