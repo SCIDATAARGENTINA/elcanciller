489a1de6c95d82c210fca85cbf46d51fa461ea45
Lucas <lucas@scidata.com.ar>
2019-10-15 12:04:57 -0300

    asd1

--- wp-content/themes/elcanciller/template-parts/home/trending-front.php
+++ wp-content/themes/elcanciller/template-parts/home/trending-front.php
@@ -41,7 +41,7 @@
 </div>
 <div class="ad-long">
 
-   <div id="publicidadCABANota">
+   <div id="publicidadCABANota" style="width: 100% !important;">
       <a href="https://www.buenosaires.gob.ar/salud">
          <img src="http://elcanciller.com/wp-content/uploads/2019/10/CABAdesktop1.jpeg" class="onlydesktop" style="width: 728px;">
          <img src="http://elcanciller.com/wp-content/uploads/2019/10/CABAmobile1.jpeg" class="onlymobile" style="width: 300px;">
